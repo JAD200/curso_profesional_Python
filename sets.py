6d33ebc322859cfb7ca9a4121954cb07bf1b4ab5
Juan Di Pasquo <juandipasquo@gmail.com>
2021-11-17 20:21:13 -0300

    Short version of set

--- sets.py
+++ sets.py
@@ -1,5 +1,7 @@
 def remove_duplicates(some_list):
+#*   This is a set:
     return list(set(some_list))
+#*   this is the same as that set but with for
     # without_duplicates = []
     # for element in some_list:
     #     if element not in without_duplicates:
@@ -7,8 +9,8 @@ def remove_duplicates(some_list):
     # return without_duplicates
 
 def run():
-    some_list = [ 11, 55, 2, 2, 55]
-    print(remove_duplicates(some_list))
+    random_list = [ 11, 55, 2, 2, 55]
+    print(remove_duplicates(random_list))
 
 
 if __name__ == '__main__':
