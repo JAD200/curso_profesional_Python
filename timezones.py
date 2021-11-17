ae9c1af4e00c07eca2233f601590de80afa96959
Juan Di Pasquo <juandipasquo@gmail.com>
2021-11-17 20:55:15 -0300

    Timezones and requirements

---
+++ timezones.py
@@ -0,0 +1,14 @@
+from datetime import date, datetime
+import pytz
+
+bogota_timezone = pytz.timezone("America/Bogota")
+bogota_date = datetime.now(bogota_timezone)
+print('Bogota: ', bogota_date.strftime('%d/%m/%Y, %H:%M:%S'))
+
+mexico_timezone = pytz.timezone("America/Mexico_City")
+mexico_date = datetime.now(mexico_timezone)
+print('Ciudad de Mexico: ', mexico_date.strftime('%d/%m/%Y, %H:%M:%S'))
+
+caracas_timezone = pytz.timezone("America/Caracas")
+caracas_date = datetime.now(caracas_timezone)
+print('Caracas: ', caracas_date.strftime('%d/%m/%Y, %H:%M:%S'))
